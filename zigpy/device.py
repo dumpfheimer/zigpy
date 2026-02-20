6bb69aacfdbd269148a36ddd3cc986bee439e0cd
Christopher Hoch <chris.hoch@hpc.at>
2026-02-20 14:51:48 +0100

    cleaned up topology route and added two-hop routes

--- zigpy/device.py
+++ zigpy/device.py
@@ -1099,7 +1099,7 @@ class Device(zigpy.util.LocalLogMixin, zigpy.util.ListenableMixin):
                     ping=True,
                     route=route,
                 )
-                if ret == zdo_t.Status.SUCCESS:
+                if ret == zdo_t.Status.SUCCESS or ret == zdo_t.Status.DEVICE_NOT_FOUND:
                     LOGGER.debug("Ping to %s succeeded using route %s: %s", self.nwk, route, ret)
                     return True
                 else:
